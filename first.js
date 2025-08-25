bac0029b9c9e36d5c6ca4616324943a29c0d399c
shahzaib-shafique <shahzaibshafique734@gmail.com>
2025-08-25 08:37:02 -0700

    JS learning

---
+++ first.js
@@ -0,0 +1,106 @@
+/*fullName="Shahzaib shafique";
+age=21;
+price=99.9;
+console.log(fullName);
+console.log(age);
+console.log(price);
+x=null;
+y=undefined;
+console.log(x);
+console.log(y);
+isFollow=true;
+console.log(isFollow);
+isnotFollow=false;
+console.log(isnotFollow);
+Name=25;
+console.log(Name);
+
+lastname="Aman";
+LASTNAME="Basit";
+console.log(lastname);
+console.log(LASTNAME);
+
+//console="Shahzaib Shafique"  fixed or reserved words cant be used as a vaiable// 
+
+let fullName="Tech Titans";
+let age=21;
+let totalPrice=999.9;
+const PI=3.14;
+console.log(fullName);
+console.log(age);
+console.log(totalPrice);
+console.log(PI);
+
+let a;
+console.log(a);
+/*const a; this will create error because its fixed so first time it initialize with some value one time*/
+
+/* block  
+{
+    let a=4;
+    /*let a=4; this can't be redeclared cause an error but updated
+    console.log(a);
+}
+{
+    let a=5;
+   /*but this time in the seperated block it can be declared and to avoid from redeclaration error
+    console.log(a);
+}
+
+const student={
+    name:"Shahzaib Shafique",
+    age:21,
+    CGPA:3.2,
+    isPass:true
+};
+console.log(student);
+
+student["age"]=student["age"]+1;
+console.log(student["age"]);
+console.log(student.name);
+
+//Making different object(collection of different keys along with their Values)
+
+//Q1//
+//const Pen={
+    fullName:"Parker Jotter Standard CT Ball Pen",
+    color:"Black",
+    rating:7002,
+    offer:5,
+    fullPrice:270,
+    isoffer:true
+};
+console.log(Pen);
+console.log(Pen["color"]);
+console.log(Pen.isoffer);  // if "abc"+1 is given then output is abc1 //
+                            // if "1"+1 is given then output is 11
+//Q2//
+const Profile={
+fullName:"shradhaKhapra",
+nPosts:195,
+nFollowers:569000,
+nFollowing:4,
+Role:"Entrepreneur",
+isFollow:true
+};
+console.log(Profile);
+console.log(Profile.fullName)
+console.log(Profile["isFollow"]);
+console.log(Profile.Role);
+console.log(Profile["nFollowers"]);
+
+console.log(typeof Profile["Role"]);
+console.log(typeof Profile["isFollow"]);
+console.log(typeof Profile["nFollowing"]);
+console.log(typeof Profile["nFollowers"]);
+console.log(typeof Profile["nPosts"]);
+console.log(typeof Profile["fullName"]);*/
+
+ 
+ 
+
+
+
+
+
+
